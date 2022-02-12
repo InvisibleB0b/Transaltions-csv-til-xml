921e4df70959127c8db58ec6302d12de67dcbfdd
InvisibleB0b <theodor.per@gmail.com>
2022-02-12 16:24:27 +0100

    Final changes to Dynamicweb translator

--- index.js
+++ index.js
@@ -4,19 +4,25 @@
 const fs = require("fs");
 csv = fs.readFileSync("CSV_file.csv")
 
-const xmlFile = fs.readFileSync("Translations.xml");
+let xmlFile = "";
+
+try {
+    console.log('Reading translations from xml...');
+    xmlFile = fs.readFileSync("Translations.xml");
+    console.log('Translations are loaded');
+} catch (error) {
+    console.log('No Translationsfile Found Remember to insert existing translations file to update all translations');
+}
 
 const xmlContent = xmlFile.toString();
 
 const reg = /<key [^>]+>(\n|[^\n](?!<\/key>))+/gm;
 
+console.log('Reading all keys...');
 const matchedKeys = xmlContent.match(reg);
 
-//REMOVE:
-console.log('matchedKeys', matchedKeys[0].toString());
-
-
-let existingTranslations = matchedKeys.reduce((acc, curr) => {
+console.log('Generating translations from existing ones');
+let existingTranslations = matchedKeys?.reduce((acc, curr) => {
     let translation = {
         Key: /name=\"([^\"]+)/gm.exec(curr)[1],
         Default: /DefaultValue=\"([^\"]+)/gm.exec(curr)[1]
@@ -25,21 +31,31 @@ let existingTranslations = matchedKeys.reduce((acc, curr) => {
     curr = curr.replace(/\r|\n/g, '');
     let allTranslationsStart = curr.match(/<translation[^>]+>/g);
 
+    allTranslationsStart.forEach((current) => {
+
+        const translationIndex = curr.indexOf(current);
+
+        const endTranslationIndex = curr.indexOf('</translation>', translationIndex);
+
+        const entireTranslationTag = curr.substring(translationIndex, endTranslationIndex + 14);
+
+        const theCulture = /culture=\"([^\"]+)/gm.exec(current)[1];
+
+        const theTranslatedValue = /\<\!\[CDATA\[([^\]]*)\]\]>/gm.exec(entireTranslationTag);
+
+        translation[theCulture] = theTranslatedValue != null ? theTranslatedValue[1] : "";
+
+    });
+
     acc.push(translation);
     return acc;
 }, [])
 
-//REMOVE:
-// console.log('ex', existingTranslations[0]);
 
-
-
-// Convert the data to String and
-// split it in an array
 var array = csv.toString().split("\r\n");
 
+console.log('Generating translations from csv file');
 const headers = array[0].split(";").map((el) => el.replace(/^\uFEFF/gm, ""));
-//remove headers and empty rows
 array.shift();
 array = array.filter(el => el != '');
 
@@ -57,11 +73,27 @@ array = array.reduce((accu, current) => {
 
 }, []);
 
+console.log('Comparing translations and updateing... skipped if no exsiting translations are present');
+let translationsToUpdate = array.filter((csvTranslation) => existingTranslations?.some(xmlTranslation => xmlTranslation.Key == csvTranslation.Key));
+
+let translationsToAdd = array.filter((csvTranslation) => !existingTranslations?.some(xmlTranslation => xmlTranslation.Key == csvTranslation.Key));
+
+translationsToUpdate.forEach((updatedTranslation) => {
+
+    const translationIndexInExisting = existingTranslations.findIndex((xmlTranslation) => xmlTranslation.Key == updatedTranslation.Key);
+
+    existingTranslations[translationIndexInExisting] = updatedTranslation;
+});
+
+let combinedTranslations = [...existingTranslations ?? new Array, ...translationsToAdd];
+
+
+// generating the translations xml
 let resultArray = `<?xml version="1.0" encoding="utf-8"?>
 <translations>
 `
-
-resultArray += array.reduce((accu, current) => {
+console.log('Converting to xml');
+resultArray += combinedTranslations.reduce((accu, current) => {
     const keys = Object.keys(current).filter(el => el != 'Key' && el != 'Default');
 
     const insertedXML = `<key name="${current.Key}" DefaultValue="${current.Default}">
@@ -76,4 +108,8 @@ resultArray += array.reduce((accu, current) => {
 
 resultArray += '</translations>';
 
-fs.writeFileSync('newTranslations.xml', resultArray)
+console.log('Saveing as xml file');
+fs.writeFileSync('newTranslations.xml', resultArray);
+
+console.log("---------DONE---------");
+console.log(`${translationsToUpdate.length} translations updated \n${translationsToAdd.length} translations added \n${xmlFile != "" ? existingTranslations.length - translationsToUpdate.length + " translations retained" : ""}`);
